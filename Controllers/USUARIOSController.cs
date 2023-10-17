27ceb704426678c4562f6d941c95fe76df3f24d7
delarocafrankUMG <fdelarocag@miumg.edu.gt>
2023-10-16 21:53:21 -0600

    Pantalla de Usuarios

---
+++ Controllers/USUARIOSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoProgra2.Models;
+
+namespace ProyectoProgra2.Controllers
+{
+    public class USUARIOSController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: USUARIOS
+        public ActionResult Index()
+        {
+            return View(db.USUARIOS.ToList());
+        }
+
+        // GET: USUARIOS/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: USUARIOS/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "USUARIO,CONTRASENA")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIOS.Add(uSUARIOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // POST: USUARIOS/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "USUARIO,CONTRASENA")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // POST: USUARIOS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            db.USUARIOS.Remove(uSUARIOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
